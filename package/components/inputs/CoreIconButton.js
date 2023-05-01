3702e936d65e31ac9827eac5803cfb5cde02a1d1
sumanta-m <sumanta.m@vixplor.com>
2023-05-01 13:44:30 +0530

    native icon button in core icon button

--- package/components/inputs/CoreIconButton.js
+++ package/components/inputs/CoreIconButton.js
@@ -1,23 +1,6 @@
 import React from "react";
-import { SCIconButton } from "@wrappid/styled-components";
-import CoreTooltip from "../dataDisplay/CoreTooltip";
+import { NativeIconButton } from "@wrappid/styled-components";
 
 export default function CoreIconButton(props) {
-  const {
-    title,
-    titlePlacement = "bottom",
-    size = "small",
-    ...restProps
-  } = props;
-  return (
-    <>
-      {title ? (
-        <CoreTooltip title={title} arrow placement={titlePlacement}>
-          <SCIconButton {...restProps} size={size} />
-        </CoreTooltip>
-      ) : (
-        <SCIconButton {...restProps} size={size} />
-      )}
-    </>
-  );
+  return <NativeIconButton {...props} />;
 }
