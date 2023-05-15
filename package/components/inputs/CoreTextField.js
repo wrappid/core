914fc385d1c20d0922857961d6b12c5419ffaa8e
sumanta-m <sumanta.m@vixplor.com>
2023-05-15 20:28:43 +0530

    core text field issue

--- package/components/inputs/CoreTextField.js
+++ package/components/inputs/CoreTextField.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { SCTextField } from "@wrappid/styled-components";
+import { NativeTextField } from "@wrappid/styled-components";
 //do not use this component directly this is used for datepicker only internally
 export default function CoreTextField(props) {
-  return <SCTextField variant="standard" {...props} />;
+  return <NativeTextField variant="standard" {...props} />;
 }
