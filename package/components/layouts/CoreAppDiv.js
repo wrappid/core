811171a5bdd661f46b553841a73e04063a6a5edd
sumanta-m <sumanta.m@vixplor.com>
2023-05-18 13:15:51 +0530

    authprops passid in core app div

--- package/components/layouts/CoreAppDiv.js
+++ package/components/layouts/CoreAppDiv.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { NativeAppDiv } from "@wrappid/styled-components";
+import { useSelector } from "react-redux";
 
 export default function CoreAppDiv(props) {
-  return <NativeAppDiv {...props} />;
+  let uid = useSelector((state) => state?.auth?.uid);
+  return <NativeAppDiv {...props} uid={uid} />;
 }
