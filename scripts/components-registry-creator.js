28809430f59ba260a9cee7afe1a14bd39f763cfc
techoneel <contact.techoneel@gmail.com>
2023-06-14 13:10:53 +0530

    + requirements of node script for core components registry

---
+++ scripts/components-registry-creator.js
@@ -0,0 +1,24 @@
+/**
+ * To run this file
+ * from any folder
+ * node <path to this script>
+ */
+console.log("This script will generate all core components.");
+
+/**
+ * @todo
+ * 
+ * What this script will do
+ * 1. create a CoreComponentsRegistry.js 
+ * package/components/CoreComponentsRegistry.js
+ * 2. get all the components files from the components folder
+ * 3. the structure of the file is
+ * 
+ * import TestComponent from "./components/TestComponent";
+ * export const ComponentRegistry = {
+ *     TestComponents: {
+ *         comp: TestComponent,
+ *     }
+ * }
+ *  
+ */
