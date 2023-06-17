d1f32a48fa5e0ff0540a0ed19283bb21351d8690
sumanta-m <sumanta.m@vixplor.com>
2023-06-17 20:11:47 +0530

    right drawer correction

--- package/components/surfaces/CoreRightDrawer.js
+++ package/components/surfaces/CoreRightDrawer.js
@@ -1,11 +1,21 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { NativeRightDrawer } from "@wrappid/styled-components";
+import { toggleRightMenuState } from "../../store/action/menuAction";
 
 export default function CoreRightDrawer(props) {
+  const dispatch = useDispatch();
   const rightMenuOpen = useSelector((state) => state?.menu?.rightMenuOpen);
+  const toggleRightDrawer = () => {
+    dispatch(toggleRightMenuState());
+  };
   return (
-    <NativeRightDrawer open={rightMenuOpen} {...props}>
+    <NativeRightDrawer
+      onOpen={toggleRightDrawer}
+      onClose={toggleRightDrawer}
+      open={rightMenuOpen}
+      {...props}
+    >
       {props.children}
     </NativeRightDrawer>
   );
