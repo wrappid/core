ef01a62feef09dba3d2e2cfeeffd32a7d4b3d023
Aditya Jaiswal <aditya.jaiswal.cse23@heritageit.edu.in>
2023-07-10 13:16:23 +0530

    + minor change

--- package/components/feedback/CoreRequestProgressBar.js
+++ package/components/feedback/CoreRequestProgressBar.js
@@ -5,11 +5,10 @@ import { useSelector } from "react-redux";
 export default function CoreRequestProgressBar() {
   const requestProgress = useSelector((state) => state.app.requestProgress);
 
-  console.log("visible value: ", requestProgress.visible);
   return (
     <>
       {requestProgress.visible ? (
-        <CoreLinearProgress indeterminate={true} color={"red"} />
+        <CoreLinearProgress />
       ) : null}
     </>
   );
