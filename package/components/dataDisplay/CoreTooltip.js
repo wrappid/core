3ab0e08f4441dc04b857fd4237a7d063e0cbc334
Aditya Jaiswal <aditya.jaiswal.cse23@heritageit.edu.in>
2023-07-13 15:56:59 +0530

    + passed props in NativeToolTip component

--- package/components/dataDisplay/CoreTooltip.js
+++ package/components/dataDisplay/CoreTooltip.js
@@ -2,5 +2,5 @@ import { NativeTooltip } from "@wrappid/styled-components";
 import React from "react";
 
 export default function CoreTooltip(props) {
-  return <NativeTooltip />;
+  return <NativeTooltip {...props} />;
 }
