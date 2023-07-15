307060ccbbccfbd073d135e3f98b00487611a74a
techoneel <contact.techoneel@gmail.com>
2023-07-15 18:40:17 +0530

    + auth container design fix and code clean up

--- package/modules/auth/components/AuthContainer.js
+++ package/modules/auth/components/AuthContainer.js
@@ -8,13 +8,6 @@ import { nativeUseNavigate } from "@wrappid/styled-components";
 import { useSelector } from "react-redux";
 import CoreComponent from "../../../components/CoreComponent";
 import { CoreResourceContext } from "../../../config/contextHandler";
-import CoreApiVersion from "../../../components/utils/custom/CoreApiVersion";
-import CoreAppVersion from "../../../components/utils/custom/CoreAppVersion";
-import CoreTypographyCaption from "../../../components/dataDisplay/paragraph/CoreTypographyCaption";
-import CoreGrid from "../../../components/layouts/CoreGrid";
-import CoreLink from "../../../components/navigation/CoreLink";
-import config from "../../../config/config";
-import CoreDivider from "../../../components/dataDisplay/CoreDivider";
 
 export const AuthContainer = (props) => {
   const navigate = nativeUseNavigate();
@@ -62,61 +55,7 @@ export const AuthContainer = (props) => {
         >
           <CoreComponent componentName="AppLogo" />
         </CoreBox>
-
         {props.children}
-
-        {/* <CoreDivider variant="middle" /> */}
-
-        <CoreGrid
-          styleClasses={[
-            CoreClasses.ALIGNMENT.JUSTIFY_CONTENT_SPACE_BETWEEN,
-            CoreClasses.MARGIN.MT2,
-          ]}
-        >
-          <CoreBox
-            gridProps={{ gridSize: 6 }}
-            styleClasses={[CoreClasses.ALIGNMENT.JUSTIFY_CONTENT_FLEX_START]}
-          >
-            <CoreAppVersion noTitle={true} />
-          </CoreBox>
-          <CoreBox
-            gridProps={{ gridSize: 6 }}
-            styleClasses={[
-              CoreClasses.LAYOUT.FULL_WIDTH,
-              CoreClasses.FLEX.DIRECTION_ROW,
-              CoreClasses.ALIGNMENT.ALIGN_ITEMS_CENTER,
-              CoreClasses.ALIGNMENT.JUSTIFY_CONTENT_FLEX_END,
-              // CoreClasses.ALIGNMENT.JUSTIFY_CONTENT_SPACE_BETWEEN,
-            ]}
-          >
-            {/* <CoreLink
-              styleClasses={[CoreClasses?.MARGIN?.MR1]}
-              href={
-                process.env?.REACT_APP_WRAPPID_helpLink ||
-                config?.wrappid?.helpLink
-              }
-            >
-              Help
-            </CoreLink> */}
-            <CoreLink
-              styleClasses={[CoreClasses?.MARGIN?.MR1]}
-              href={
-                process.env?.REACT_APP_WRAPPID_privacyLink ||
-                config?.wrappid?.privacyLink
-              }
-            >
-              Privacy
-            </CoreLink>
-            <CoreLink
-              href={
-                process.env?.REACT_APP_WRAPPID_termsLink ||
-                config?.wrappid?.termsLink
-              }
-            >
-              Terms
-            </CoreLink>
-          </CoreBox>
-        </CoreGrid>
       </CoreSection>
     </CoreBox>
   );
