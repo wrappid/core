bef9537034bd9d75d75076375a621828c17ac91a
Aditya Jaiswal <aditya.jaiswal.cse23@heritageit.edu.in>
2023-07-18 16:28:38 +0530

    #28
    - Added support for CoreLink
    - Added colors to icon and text

--- package/components/dataDisplay/custom/CoreIconText.js
+++ package/components/dataDisplay/custom/CoreIconText.js
@@ -2,15 +2,26 @@ import React from "react";
 import CoreClasses from "../../../styles/CoreClasses";
 import CoreIcon from "../CoreIcon";
 import CoreTypographyCaption from "../paragraph/CoreTypographyCaption";
+import CoreBox from "../../layouts/CoreBox";
+import CoreLink from "../../navigation/CoreLink";
 
 export default function CoreIconText(props) {
-  const { icon, text, type } = props;
+  const { icon, text, type, link = false, href } = props;
 
   return (
-    <CoreTypographyCaption styleClasses={[CoreClasses.ALIGNMENT.ALIGN_ITEMS_START]}>
-      <CoreIcon type={type} style={{ fontSize: 12 }}>{icon}</CoreIcon>
+    (link === true)
+    ?
+      <CoreBox styleClasses={[CoreClasses.ALIGNMENT.JUSTIFY_CONTENT_CENTER]}>
+        <CoreIcon type={type} style={{ fontSize: 12 }}>{icon}</CoreIcon>
+
+        {/* @todo need to change the "style" below */}
+        <CoreLink href={href || "#"} style={{ fontSize: 12 }} styleClasses={[CoreClasses.MARGIN.ML1]}>{text}</CoreLink>
+      </CoreBox>
+    :
+    <CoreTypographyCaption styleClasses={[CoreClasses.ALIGNMENT.JUSTIFY_CONTENT_CENTER]}>
+      <CoreIcon styleClasses={[CoreClasses.COLOR.TEXT_PRIMARY]} type={type} style={{ fontSize: 12 }}>{icon}</CoreIcon>
       
-      <CoreTypographyCaption styleClasses={[CoreClasses.MARGIN.ML1]}>{text}</CoreTypographyCaption>
+      <CoreTypographyCaption styleClasses={[CoreClasses.MARGIN.ML1, CoreClasses.COLOR.TEXT_SECONDARY]}>{text}</CoreTypographyCaption>
     </CoreTypographyCaption>
   );
 }
