46cf6a32f636c57dc5c54ac781ffb116536a7585
techoneel <contact.techoneel@gmail.com>
2023-08-07 16:57:51 +0530

    #44 issue fix

--- package/components/dataDisplay/custom/UserChip.js
+++ package/components/dataDisplay/custom/UserChip.js
@@ -1,14 +1,16 @@
 import React from "react";
-import config from "../../../config/config";
+
+import { getConfigurationObject } from "@wrappid/styles";
+
 import { HTTP } from "../../../config/constants";
 import axiosInterceptor from "../../../middleware/axiosInterceptor";
 import authHeader from "../../../service/DataService";
+import CoreClasses from "../../../styles/CoreClasses";
 import { getFullName } from "../../../utils/helper";
 import CoreAvatar from "../CoreAvatar";
 import CoreChip from "../CoreChip";
-import CoreTypographyCaption from "../paragraph/CoreTypographyCaption";
-import CoreClasses from "../../../styles/CoreClasses";
 import CoreTooltip from "../CoreTooltip";
+import CoreTypographyCaption from "../paragraph/CoreTypographyCaption";
 
 export default function UserChip(props) {
   const {
@@ -26,17 +28,19 @@ export default function UserChip(props) {
   const [email, setEmail] = React.useState(_email);
   const [photoUrl, setPhotoUrl] = React.useState(_photoUrl);
 
+  let appConfig = getConfigurationObject();
+
   React.useEffect(() => {
     async function apiCall() {
-      let backendUrl =
-        process.env.REACT_APP_WRAPPID_backendUrl || config.wrappid.backendUrl;
+      let backendUrl = appConfig?.wrappid?.backendUrl;
+
       await axiosInterceptor({
-        method: HTTP.GET,
+        headers: await authHeader(true),
+        method : HTTP.GET,
         url:
           backendUrl +
           "/business/individual/UserBasicInfo?_defaultFilter=" +
           encodeURIComponent(JSON.stringify({ id: userid })),
-        headers: await authHeader(true),
       })
         .then((response) => {
           setFirstName(response?.data?.data?.data["Person.firstName"]);
@@ -46,7 +50,7 @@ export default function UserChip(props) {
           setPhotoUrl(response?.data?.data?.data["Person.photoUrl"]);
         })
         .catch((error) => {
-          console.error(error);
+          throw error;
         });
     }
     if (
@@ -61,10 +65,11 @@ export default function UserChip(props) {
   }, [userid]);
 
   let displayName = "";
+
   if (firstName === "" && middleName === "" && lastName === "") {
     displayName = "Unknown";
   } else {
-    displayName = getFullName({ firstName, middleName, lastName });
+    displayName = getFullName({ firstName, lastName, middleName });
   }
 
   return titleVisible ? (
@@ -78,13 +83,13 @@ export default function UserChip(props) {
         />
       }
       label={
-        <CoreTypographyCaption limitChars={6} hideSeeMore={true}>
-          {displayName}
+        <CoreTypographyCaption hideSeeMore={true}>
+          {firstName}
         </CoreTypographyCaption>
       }
     />
   ) : (
-    <CoreTooltip title={displayName} arrow>
+    <CoreTooltip title={displayName}>
       <CoreAvatar
         styleClasses={[CoreClasses.DATA_DISPLAY.AVATAR_SMALL]}
         src={photoUrl || "photo.jpg"}
