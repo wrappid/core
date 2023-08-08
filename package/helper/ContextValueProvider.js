9ec486d65dbf1d010245aa65e6bb2c2b0a6b5b53
techoneel <contact.techoneel@gmail.com>
2023-08-08 11:42:11 +0530

    Context Related Updates
    + urlRegistry, apiRegistry introduced in application context
    + context value provider introduced

--- package/helper/ContextValueProvider.js
+++ package/helper/ContextValueProvider.js
@@ -3,9 +3,7 @@ import React from "react";
 import { FunctionsRegistryContext } from "../config/contextHandler";
 
 export const getFunctions = () => {
-
   const functionsRegistry = React.useContext(FunctionsRegistryContext);
     
   return functionsRegistry;
-
 };
