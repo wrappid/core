e40886370b2ab30abf2c5f0869728f8a48ed7790
sumanta-m <sumanta.m@vixplor.com>
2023-08-08 20:36:22 +0530

    error icon change

--- package/components/inputs/CoreFormErrorText.js
+++ package/components/inputs/CoreFormErrorText.js
@@ -1,15 +1,19 @@
-import React from "react";
-import CoreFormHelperText from "./CoreFormHelperText";
-import CoreIcon from "../dataDisplay/CoreIcon";
-import CoreClasses from "../../styles/CoreClasses";
+import React from 'react';
+import CoreFormHelperText from './CoreFormHelperText';
+import CoreIcon from '../dataDisplay/CoreIcon';
+import CoreClasses from '../../styles/CoreClasses';
 
 export default function CoreFormErrorText(props) {
   return (
-    <CoreFormHelperText {...props} styleClasses={[...(props.styleClasses||[]), CoreClasses.ALIGNMENT.ALIGN_ITEMS_CENTER]} error={true}>
-      <CoreIcon styleClasses={[CoreClasses.MARGIN.MR1]}>
-        info
-      </CoreIcon>
+    <CoreFormHelperText
+      {...props}
+      styleClasses={[
+        ...(props.styleClasses || []),
+        CoreClasses.ALIGNMENT.ALIGN_ITEMS_CENTER,
+      ]}
+      error={true}>
+      <CoreIcon styleClasses={[CoreClasses.MARGIN.MR1]}>error</CoreIcon>
       {props.children}
     </CoreFormHelperText>
-  )
+  );
 }
