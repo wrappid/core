8c55d3bc0dc818072598bc5f29b9be0fda607915
techoneel <contact.techoneel@gmail.com>
2023-08-12 21:15:06 +0530

    + coretableaction react component

--- package/components/dataTable/CoreTableAction.js
+++ package/components/dataTable/CoreTableAction.js
@@ -1,3 +1,6 @@
+// eslint-disable-next-line unused-imports/no-unused-imports, no-unused-vars
+import React from "react";
+
 // eslint-disable-next-line import/no-unresolved
 import { nativeUseNavigate } from "@wrappid/styled-components";
 
