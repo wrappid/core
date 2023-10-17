b404309086b74f045b3345122401d2a1d79b0c3f
techoneel <contact.techoneel@gmail.com>
2023-10-17 13:47:25 +0530

    + added valid invalid props

--- package/components/dataDisplay/heading/CoreH1.js
+++ package/components/dataDisplay/heading/CoreH1.js
@@ -2,17 +2,93 @@
 import React from "react";
 
 import CoreH from "./CoreH";
+import { sanitizeComponentProps } from "../../../utils/componentUtil";
 
 export default function CoreH1(props) {
-  const { styleClasses = [], variant = "h1", children, ...restProps } = props;
+  props = sanitizeComponentProps(CoreH1, props);
+  const {
+    align, gutterBottom, noWrap, paragraph, component, variant = "h1", children 
+  } = props;
   
   return (
     <CoreH
-      styleClasses={styleClasses}
+      align={align}
+      gutterBottom={gutterBottom}
+      noWrap={noWrap}
+      paragraph={paragraph}
+      component={component}
       variant={variant}
-      {...restProps}
       __level="h1">
       {children}
     </CoreH>
   );
 }
+
+CoreH1.validProps = [
+  {
+    description: "Set the text-align on the component.",
+    name       : "align",
+    types      : [
+      {
+        default    : "inherit",
+        type       : "string",
+        validValues: [
+          "center",
+          "inherit",
+          "justify",
+          "left",
+          "right"
+        ]
+      }
+    ]
+  },
+  {
+    description: "If true, the text will have a bottom margin.",
+    name       : "gutterBottom",
+    types      : [
+      {
+        default    : false,
+        type       : "boolean",
+        validValues: [true, false]
+      }
+    ]
+  },
+  {
+    description: "If true, the text will not wrap, but instead will truncate with a text overflow ellipsis. Note that text overflow can only happen with block or inline-block level elements (the element needs to have a width in order to overflow).",
+    name       : "noWrap",
+    types      : [
+      {
+        default    : false,
+        type       : "boolean",
+        validValues: [true, false]
+      }
+    ]
+  },
+  {
+    description: "If true, the element will be a paragraph element.",
+    name       : "paragraph",
+    types      : [
+      {
+        default    : false,
+        type       : "boolean",
+        validValues: [true, false]
+      }
+    ]
+  },
+  {
+    description: "The component used for the root node. Either a string to use a HTML element or a component.",
+    name       : "component",
+    types      : [{ type: "string" }]
+  },
+  {
+    description: "Applies the theme typography styles.",
+    name       : "variant",
+    types      : [
+      {
+        default: "h1",
+        type   : "string"
+      }
+    ]
+  }
+];
+CoreH1.invalidProps = ["sx", "classes"];
