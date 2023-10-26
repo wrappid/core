78460eb2ff6f5b9f5221826b84cca4d10511b26f
Ananta KumarGhosh <antaghosh@gmail.com>
2023-10-26 16:25:07 +0530

    + fix props

--- package/components/feedback/CoreSnackbar.js
+++ package/components/feedback/CoreSnackbar.js
@@ -48,30 +48,30 @@ export default function CoreSnackbar(props) {
     </NativeSnackbar>
   );
 }
-CoreSnackbar.invalidProps = [
+CoreSnackbar.validProps = [
   {
     description:
       "The action to display. It renders after the message, at the end of the snackbar.",
-    name: "action",
+    name : "action",
     types: [
       {
         default: "",
-        type: "node",
+        type   : "node",
       },
     ],
   },
   {
     description:
       "The anchor of the Snackbar. On smaller screens, the component grows to occupy all the available width, the horizontal alignment is ignored.",
-    name: "anchorOrigin",
+    name : "anchorOrigin",
     types: [
       {
-        default: { vertical: "bottom", horizontal: "left" },
-        type: "object",
+        default    : { horizontal: "left", vertical: "bottom" },
+        type       : "object",
         validValues: [
           {
             horizontal: ["center", "left", "right"],
-            vertical: ["bottom", "top"],
+            vertical  : ["bottom", "top"],
           },
         ],
       },
@@ -80,73 +80,73 @@ CoreSnackbar.invalidProps = [
   {
     description:
       "The number of milliseconds to wait before automatically calling the onClose function. onClose should then set the state of the open prop to hide the Snackbar. This behavior is disabled by default with the null value.",
-    name: "autoHideDuration",
+    name : "autoHideDuration",
     types: [
       {
         default: "null",
-        type: "number",
+        type   : "number",
       },
     ],
   },
   {
     description: "Replace the SnackbarContent component.",
-    name: "children",
-    types: [
+    name       : "children",
+    types      : [
       {
         default: "",
-        type: "element",
+        type   : "element",
       },
     ],
   },
   {
     description: "Props applied to the ClickAwayListener element.",
-    name: "ClickAwayListenerProps",
-    types: [
+    name       : "ClickAwayListenerProps",
+    types      : [
       {
         default: "",
-        type: "object",
+        type   : "object",
       },
     ],
   },
   {
     description: "Props applied to the SnackbarContent element.",
-    name: "ContentProps",
-    types: [
+    name       : "ContentProps",
+    types      : [
       {
         default: "",
-        type: "object",
+        type   : "object",
       },
     ],
   },
   {
     description:
       "If true, the autoHideDuration timer will expire even if the window is not focused.",
-    name: "disableWindowBlurListener",
+    name : "disableWindowBlurListener",
     types: [
       {
         default: "FALSE",
-        type: "bool",
+        type   : "bool",
       },
     ],
   },
   {
     description:
       "When displaying multiple consecutive snackbars using a single parent-rendered <Snackbar/>, add the key prop to ensure independent treatment of each message. For instance, use <Snackbar key={message} />. Otherwise, messages might update in place, and features like autoHideDuration could be affected.",
-    name: "key",
+    name : "key",
     types: [
       {
         default: "",
-        type: "any",
+        type   : "any",
       },
     ],
   },
   {
     description: "The message to display.",
-    name: "message",
-    types: [
+    name       : "message",
+    types      : [
       {
         default: "",
-        type: "node",
+        type   : "node",
       },
     ],
   },
@@ -154,50 +154,50 @@ CoreSnackbar.invalidProps = [
     description: `Callback fired when the component requests to be closed. Typically onClose is used to set state in the parent component, which is used to control the Snackbar open prop. The reason parameter can optionally be used to control the response to onClose, for example ignoring clickaway.
     Signature:function(event: React.SyntheticEvent | Event, reason: string) => voidevent The event source of the callback.
     reason Can be: ""timeout"" (autoHideDuration expired), ""clickaway"", or ""escapeKeyDown"".`,
-    name: "onClose",
+    name : "onClose",
     types: [
       {
         default: "",
-        type: "func",
+        type   : "func",
       },
     ],
   },
   {
     description: "If true, the component is shown.",
-    name: "open",
-    types: [
+    name       : "open",
+    types      : [
       {
         default: "",
-        type: "bool",
+        type   : "bool",
       },
     ],
   },
   {
     description:
       "The number of milliseconds to wait before dismissing after user interaction. If autoHideDuration prop isn't specified, it does nothing. If autoHideDuration prop is specified but resumeHideDuration isn't, we default to autoHideDuration / 2 ms.",
-    name: "resumeHideDuration",
+    name : "resumeHideDuration",
     types: [
       {
         default: "",
-        type: "string",
+        type   : "string",
       },
     ],
   },
   {
     description:
       "The component used for the transition. Follow this guide to learn more about the requirements for this component.",
-    name: "TransitionComponent",
+    name : "TransitionComponent",
     types: [
       {
         default: "Grow",
-        type: "string",
+        type   : "string",
       },
     ],
   },
   {
     description:
       "The duration for the transition, in milliseconds. You may specify a single timeout for all transitions, or individually with an object.",
-    name: "transitionDuration",
+    name : "transitionDuration",
     types: [
       {
         default:
@@ -211,11 +211,11 @@ CoreSnackbar.invalidProps = [
   {
     description:
       "Props applied to the transition element. By default, the element is based on this Transition component.",
-    name: "TransitionProps",
+    name : "TransitionProps",
     types: [
       {
         default: {},
-        type: "object",
+        type   : "object",
       },
     ],
   },
