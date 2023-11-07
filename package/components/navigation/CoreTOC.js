f1d8ecc833a7a8787f5283a873d5c956d84a7c47
techoneel <contact.techoneel@gmail.com>
2023-11-07 18:23:11 +0530

    + core toc basic

--- package/components/navigation/CoreTOC.js
+++ package/components/navigation/CoreTOC.js
@@ -14,112 +14,112 @@ export function getPageTOC() {
         Children: [
           {
             Children: [],
-            id      : "Menu-1.1",
-            label   : "Menu 1.1",
-            name    : "Menu 1.1",
-            type    : "menuitem"
+            id: "Menu-1.1",
+            label: "Menu 1.1",
+            name: "Menu 1.1",
+            type: "menuitem"
           },
           {
             Children: [],
-            id      : "Menu-1.2",
-            label   : "Menu 1.2",
-            name    : "Menu 1.2",
-            type    : "separator"
+            id: "Menu-1.2",
+            label: "Menu 1.2",
+            name: "Menu 1.2",
+            type: "separator"
           },
           {
             Children: [
               {
                 Children: [],
-                id      : "Menu-1.3.1",
-                label   : "Menu 1.3.1",
-                name    : "Menu 1.3.1",
-                type    : "menuitem"
+                id: "Menu-1.3.1",
+                label: "Menu 1.3.1",
+                name: "Menu 1.3.1",
+                type: "menuitem"
               },
               {
                 Children: [],
-                id      : "Menu-1.3.2",
-                label   : "Menu 1.3.2",
-                name    : "Menu 1.3.2",
-                type    : "separator"
+                id: "Menu-1.3.2",
+                label: "Menu 1.3.2",
+                name: "Menu 1.3.2",
+                type: "separator"
               },
               {
                 Children: [],
-                id      : "Menu-1.3.3",
-                label   : "Menu 1.3.3",
-                name    : "Menu 1.3.3",
-                type    : "parent"
+                id: "Menu-1.3.3",
+                label: "Menu 1.3.3",
+                name: "Menu 1.3.3",
+                type: "parent"
 
               }
             ],
-            id   : "Menu-1",
+            id: "Menu-1",
             label: "Menu 1.3",
-            name : "Menu 1.3",
-            type : "parent"
+            name: "Menu 1.3",
+            type: "parent"
 
           }
         ],
-        id   : "Menu-1",
+        id: "Menu-1",
         label: "Menu 1",
-        name : "Menu 1",
-        type : "header"
+        name: "Menu 1",
+        type: "header"
       },
       {
         Children: [
           {
             Children: [],
-            id      : "Menu-2.1",
-            label   : "Menu 2.1",
-            name    : "Menu 2.1",
-            type    : "menuitem"
+            id: "Menu-2.1",
+            label: "Menu 2.1",
+            name: "Menu 2.1",
+            type: "menuitem"
           },
           {
             Children: [],
-            id      : "Menu-2.2",
-            label   : "Menu 2.2",
-            name    : "Menu 2.2",
-            type    : "separator"
+            id: "Menu-2.2",
+            label: "Menu 2.2",
+            name: "Menu 2.2",
+            type: "separator"
           },
           {
             Children: [
               {
                 Children: [],
-                id      : "Menu-2.3.1",
-                label   : "Menu 2.3.1",
-                name    : "Menu 2.3.1",
-                type    : "menuitem"
+                id: "Menu-2.3.1",
+                label: "Menu 2.3.1",
+                name: "Menu 2.3.1",
+                type: "menuitem"
               },
               {
                 Children: [],
-                id      : "Menu-2.3.2",
-                label   : "Menu 2.3.2",
-                name    : "Menu 2.3.2",
-                type    : "separator"
+                id: "Menu-2.3.2",
+                label: "Menu 2.3.2",
+                name: "Menu 2.3.2",
+                type: "separator"
               },
               {
                 Children: [],
-                id      : "Menu-2.3.3",
-                label   : "Menu 2.3.3",
-                name    : "Menu 2.3.3",
-                type    : "parent"
+                id: "Menu-2.3.3",
+                label: "Menu 2.3.3",
+                name: "Menu 2.3.3",
+                type: "parent"
 
               }
             ],
-            id   : "Menu-2",
+            id: "Menu-2",
             label: "Menu 2.3",
-            name : "Menu 2.3",
-            type : "parent"
+            name: "Menu 2.3",
+            type: "parent"
 
           }
         ],
-        id   : "Menu-2",
+        id: "Menu-2",
         label: "Menu 2",
-        name : "Menu 2",
-        type : "header"
+        name: "Menu 2",
+        type: "header"
       }
     ];
 
     setTocMenu(sampleMenuContent);
-    
+
     return () => {
       // remove listener
     };
@@ -130,7 +130,11 @@ export function getPageTOC() {
 
 export default function CoreTOC(props) {
 
-  const { tocMenu } = props;
+  // eslint-disable-next-line no-console
+  const { tocMenu, OnMenuClick = () => { console.log("menu clicked"); } } = props;
 
-  return <CoreMenu menu={tocMenu} />;
+  return <CoreMenu
+    multiLevel={true}
+    menu={tocMenu}
+    OnMenuClick={OnMenuClick} />;
 }
