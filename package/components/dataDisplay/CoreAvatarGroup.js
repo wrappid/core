45dfe06925e3497ae1740c277ea9eb651880db77
Aiti <aitijhyasarkar.cse2020@nsec.ac.in>
2024-02-02 16:31:56 +0530

    style: :memo: Fixed prop table contents of CoreAvatarGroup
    fixed table rows of spacing and varient
    
    ref- #101

--- package/components/dataDisplay/CoreAvatarGroup.js
+++ package/components/dataDisplay/CoreAvatarGroup.js
@@ -18,43 +18,43 @@ export default function CoreAvatarGroup(props) {
 CoreAvatarGroup.validProps = [
   {
     description: "The component used for the root node. Either a string to use a HTML element or a component.",
-    name       : "component",
-    types      : [{ type: "elementType" }]
+    name: "component",
+    types: [{ type: "elementType" }]
   },
   {
     description: "The extra props for the slot components. You can override the existing props or add new ones.This prop is an alias for the slotProps prop. It's recommended to use the slotProps prop instead, as componentsProps will be deprecated in the future.",
-    name       : "componentsProps",
-    types      : [{ default: {}, type: "{ additionalAvatar?: object }" }]
+    name: "componentsProps",
+    types: [{ default: {}, type: "{ additionalAvatar?: object }" }]
   },
   {
     description: "Max avatars to show before +x.",
-    name       : "max",
-    types      : [{ default: 5, type: "number" }]
+    name: "max",
+    types: [{ default: 5, type: "number" }]
   },
   {
     description: "custom renderer of extraAvatarsSignature:function(surplus: number) => React.ReactNodesurplus number of extra avatarsReturns: custom element to display",
-    name       : "renderSurplus",
-    types      : [{ default: "", type: "function" }]
+    name: "renderSurplus",
+    types: [{ default: "", type: "function" }]
   },
   {
     description: "The extra props for the slot components. You can override the existing props or add new ones.This prop is an alias for the componentsProps prop, which will be deprecated in the future.",
-    name       : "slotProps",
-    types      : [{ default: {}, type: "object" }]
+    name: "slotProps",
+    types: [{ default: {}, type: "object" }]
   },
   {
     description: "Spacing between avatars.",
-    name       : "spacing",
-    types      : [{ default: "medium", type: "string", validValues: ["medium", "small"] }, { type: number }]
+    name: "spacing",
+    types: [{ default: "medium", type: "string", validValues: ["medium", "small", "number"] }]
   },
   {
     description: "The total number of avatars. Used for calculating the number of extra avatars.",
-    name       : "total",
-    types      : [{ default: "children.length", type: "number" }]
+    name: "total",
+    types: [{ default: "children.length", type: "number" }]
   },
   {
     description: "The variant to use.",
-    name       : "variant",
-    types      : [{ default: "circular'", type: "circular'| 'rounded'| 'square'| string" }]
+    name: "variant",
+    types: [{ default: "circular", type: ["circular", "rounded", "square", "string"] }]
   }
 ];
-CoreAvatarGroup.invalidProps = ["sx", "classes"];
+CoreAvatarGroup.invalidProps = [];
