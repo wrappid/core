fca3e4c37809eb0405b3116a1b125ed46c0ef118
Md Faisal Raza Khan <irkfaisal@gmail.com>
2024-02-17 16:05:30 +0530

    feat(core): :sparkles: change BlankLayout and added BlankLayoutPage for testing
    change BlankLayout and added BlankLayoutPage for testing
    
    ref: #130

--- package/layout/BlankLayout.js
+++ package/layout/BlankLayout.js
@@ -1,16 +1,10 @@
-import CoreTypographyBody1 from "../components/dataDisplay/CoreTypographyBody1";
-// eslint-disable-next-line import/order
 import CoreLayoutPlaceholder from "./core/CoreLayoutPlaceholder";
+import CoreClasses from "../styles/CoreClasses";
 
 export default function BlankLayout() {
   return (
-    <>
-      <CoreLayoutPlaceholder id="header" />
-
-      <CoreTypographyBody1>Any component can be added here...</CoreTypographyBody1>
-
-      <CoreLayoutPlaceholder id="content" />
-
-    </>
+    <CoreLayoutPlaceholder id={BlankLayout.PLACEHOLDER.CONTENT} styleClasses={[CoreClasses.WIDTH.W_100, CoreClasses.PADDING.P1]} />
   );
 }
+
+BlankLayout.PLACEHOLDER = { CONTENT: "content" };
--- package/layout/page/BlankLayoutPage.js
+++ package/layout/page/BlankLayoutPage.js
@@ -1,14 +1,22 @@
 import CoreTypographyBody1 from "../../components/dataDisplay/CoreTypographyBody1";
+import BlankLayout from "../BlankLayout";
 import CoreLayoutItem from "../core/CoreLayoutItem";
 
 export default function BlankLayoutPage() {
   return (
     <>
-      <CoreLayoutItem id="header" />
+      <CoreTypographyBody1>Component above layout item</CoreTypographyBody1>
 
-      <CoreTypographyBody1>Any component can be added here...</CoreTypographyBody1>
+      <CoreLayoutItem id={BlankLayout.PLACEHOLDER.CONTENT}>
+        <CoreTypographyBody1>Content for the blank layout</CoreTypographyBody1>
 
-      <CoreLayoutItem id="content" />
+        <CoreTypographyBody1>Component inside layout item</CoreTypographyBody1>
+
+        <CoreTypographyBody1>Any other component can be added here...</CoreTypographyBody1>
+      </CoreLayoutItem>
+
+      <CoreTypographyBody1>Component below layout item</CoreTypographyBody1>
+
     </>
   );
 }
