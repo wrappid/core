b901fc3fd1e42df1b1977f546573c089f20eeffe
sumanta-m <sumanta.m@vixplor.com>
2024-08-03 19:25:34 +0530

    fix(core): :bug: json editor theme related bug
    json editor theme related bug fixing with theme context
    
    ref #260

--- package/components/inputs/CoreJSONEditor.js
+++ package/components/inputs/CoreJSONEditor.js
@@ -1,9 +1,13 @@
 // eslint-disable-next-line unused-imports/no-unused-imports, no-unused-vars
-import React from "react";
+import React, { useContext } from "react";
 
 // eslint-disable-next-line import/no-unresolved
 import { NativeJSONEditor } from "@wrappid/native";
+// eslint-disable-next-line import/no-unresolved
+import { ThemeContext } from "@wrappid/styles";
 
 export default function CoreJsonEditor(props) {
-  return <NativeJSONEditor {...props} />;
+  const theme = useContext(ThemeContext);
+
+  return <NativeJSONEditor {...props} theme={theme} />;
 }
