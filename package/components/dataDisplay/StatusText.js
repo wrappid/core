1ba18e692cf8b6bc443f7da6a28dc368beae7ae2
anantakumarghosh <antaghosh@gmail.com>
2024-08-23 19:44:06 +0530

    fix(core): :bug: add valid props for StatusText
    add valid props and sanitization for StatusText
    
    ref: #313

--- package/components/dataDisplay/StatusText.js
+++ package/components/dataDisplay/StatusText.js
@@ -3,16 +3,44 @@ import React from "react";
 
 import CoreTypographyCaption from "./CoreTypographyCaption";
 import CoreClasses from "../../styles/CoreClasses";
+import { sanitizeComponentProps } from "../../utils/componentUtil";
 import { getStatusTextColorClass } from "../../utils/tableUtils";
 
 export default function StatusText(props) {
-  const { status, gridProps } = props;
+  props = sanitizeComponentProps(StatusText, props);
 
   return (
     <CoreTypographyCaption
-      gridProps={gridProps}
-      styleClasses={[getStatusTextColorClass(status || ""), CoreClasses.TEXT.TEXT_WEIGHT_BOLD, CoreClasses.TEXT.TEXT_UPPERCASE]}>
-      {status || "UNKNOWN"}
+      {...props}
+      styleClasses={[getStatusTextColorClass(props?.status || ""), CoreClasses.TEXT.TEXT_WEIGHT_BOLD, CoreClasses.TEXT.TEXT_UPPERCASE]}>
+      {props?.status || "UNKNOWN"}
     </CoreTypographyCaption>
   );
 }
+
+StatusText.validProps = [
+  {
+    name: "status",
+    types: [
+      {
+        default: "UNKNOWN",
+        type: "string",
+        validValues: [
+          "ACTIVE",
+          "DRAFT",
+          "NEW",
+          "APPROVED",
+          "PUBLISHED",
+          "INACTIVE",
+          "CHANGE_REQUESTED",
+          "REVIEW_REQUESTED",
+          "DELETED",
+          "REJECTED",
+          "UNKNOWN"
+        ]
+      }
+    ],
+  },
+];
+
+StatusText.invalidProps = [];
