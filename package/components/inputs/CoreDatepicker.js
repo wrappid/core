efed94a62a48646f767cb16afa690639a0986b2f
PritamIT2023 <pritammondal8981@gmail.com>
2024-08-30 15:04:40 +0530

    feat(core): :bug: add CoreDatepicker validProps
    add CoreDatepicker validProps
    
    Ref: #342

--- package/components/inputs/CoreDatepicker.js
+++ package/components/inputs/CoreDatepicker.js
@@ -28,6 +28,10 @@ export default function CoreDatePicker(props) {
 }
 CoreDatePicker.validProps = [
   {
+    name : "helperText",
+    types: [{ type: "string" }]
+  },
+  {
     description: "The currently selected date.",
     name       : "value",
     types      : [{ default: "", type: "string" }],
@@ -44,9 +48,10 @@ CoreDatePicker.validProps = [
     types      : [{ default: "", type: "string" }],
   },
   {
-    description: "A label that describes the content of the date picker. e.g. \"Arrival date\".",
-    name       : "label",
-    types      : [{ default: "", type: "string" }],
+    description:
+      "A label that describes the content of the date picker. e.g. \"Arrival date\".",
+    name : "label",
+    types: [{ default: "", type: "node" }],
   },
   {
     description: "Name of this input. Used as a reference in form data.",
@@ -84,6 +89,238 @@ CoreDatePicker.validProps = [
     description: "Whether the input value is invalid.",
     name       : "isInvalid",
     types      : [{ default: false, type: "boolean" }],
+  },
+  {
+    name : "autoFocus",
+    types: [{ type: "boolean" }],
+  },
+  {
+    name : "view",
+    types: [{ type: "day" }, { type: "month" }, { type: "year" }],
+  },
+  {
+    name : "closeOnSelect",
+    types: [{ type: "boolean" }],
+  },
+  {
+    name : "dayOfWeekFormatter",
+    types: [{ type: "function" }],
+  },
+  {
+    name : "disableFuture",
+    types: [
+      {
+        default: false,
+        type   : "boolean",
+      },
+    ],
+  },
+  {
+    name : "disableHighlightToday",
+    types: [
+      {
+        default: false,
+        type   : "boolean",
+      },
+    ],
+  },
+  {
+    name : "disableOpenPicker",
+    types: [
+      {
+        default: false,
+        type   : "boolean",
+      },
+    ],
+  },
+  {
+    name : "disablePast",
+    types: [
+      {
+        default: false,
+        type   : "boolean",
+      },
+    ],
+  },
+  {
+    name : "displayWeekNumber",
+    types: [{ type: "boolean" }],
+  },
+  {
+    name : "fixedWeekNumber",
+    types: [{ type: "number" }],
+  },
+  {
+    name : "formatDensity",
+    types: [{ default: "dense", type: "string", validValues: ["dense", "spacious"] }],
+  },
+  {
+    name : "inputRef",
+    types: [{ type: "ref" }],
+  },
+  {
+    name : "loading",
+    types: [{ default: false, type: "boolean" }],
+  },
+  {
+    name : "localeText",
+    types: [{ type: "object" }],
+  },
+  {
+    name : "maxDate",
+    types: [{ type: "object" }],
+  },
+  {
+    name : "minDate",
+    types: [{ type: "object" }],
+  },
+  {
+    name : "monthsPerRow",
+    types: [{ default: 3, type: "number" }],
+  },
+  {
+    name : "onAccept",
+    types: [{ type: "function" }],
+  },
+  {
+    name : "onChange",
+    types: [{ type: "function" }],
+  },
+  {
+    name : "onClose",
+    types: [{ type: "function" }],
+  },
+  {
+    name : "onMonthChange",
+    types: [{ type: "function" }],
+  },
+  {
+    name : "onError",
+    types: [{ type: "function" }],
+  },
+  {
+    name : "onOpen",
+    types: [{ type: "function" }],
+  },
+  {
+    description:
+      "Callback fired when the selected sections change.Signature:function(newValue: FieldSelectedSections) => voidnewValue The new selected sections.",
+    name : "onSelectedSectionsChange",
+    types: [{ default: "-", type: "function" }],
+  },
+  {
+    description:
+      "Callback fired on view change.Signature:function(view: TView) => voidview The new view.",
+    name : "onViewChange",
+    types: [{ default: "-", type: "function" }],
+  },
+  {
+    description:
+      "Callback fired on year change.Signature:function(year: TDate) => voidyear The new year.",
+    name : "onYearChange",
+    types: [{ default: "-", type: "function" }],
+  },
+  {
+    description: "Control the popup or dialog open state.",
+    name       : "open",
+    types      : [{ default: false, type: "boolean" }],
+  },
+  {
+    description:
+      "The default visible view. Used when the component view is not controlled. Must be a valid option from views list.",
+    name : "openTo",
+    types: [{ default: "-", defaultValue: ["day", "month", "year"], type: "string" }],
+  },
+  {
+    description: "Force rendering in particular orientation.",
+    name       : "orientation",
+    types      : [{ default: "-", type: "landscape'| 'portrait'" }],
+  },
+  {
+    description: "If true, disable heavy animations.",
+    name       : "reduceAnimations",
+    types      : [
+      {
+        default:
+          "`@media(prefers-reduced-motion: reduce)` || `navigator.userAgent` matches Android <10 or iOS <13",
+        type: "boolean",
+      },
+    ],
+  },
+  {
+    description:
+      "The date used to generate the new value when both value and defaultValue are empty.",
+    name : "referenceDate",
+    types: [
+      {
+        default:
+          "The closest valid date-time using the validation props, except callbacks like `shouldDisable<...>`.",
+        type: "object",
+      },
+    ],
+  },
+  {
+    description:
+      "Component displaying when passed loading true.Signature:function() => React.ReactNodeReturns: The node to render when loading.",
+    name : "renderLoading",
+    types: [{ type: "function" }],
+  },
+  {
+    description:
+      "The currently selected sections. This prop accepts four formats: 1. If a number is provided, the section at this index will be selected. 2. If a string of type FieldSectionType is provided, the first section with that name will be selected. 3. If \"all\"  is provided, all the sections will be selected. 4. If null is provided, no section will be selected. If not provided, the selected sections will be handled internally.",
+    name : "selectedSections",
+    types: [
+      {
+        default: "-",
+        type   : "all'| 'day'| 'empty'| 'hours'| 'meridiem'| 'minutes'| 'month'| 'seconds'| 'weekDay'| 'year'| number",
+      },
+    ],
+  },
+  {
+    description:
+      "Disable specific date.Warning: This function can be called multiple times (for example when rendering date calendar, checking if focus can be moved to a certain date, etc.). Expensive computations can impact performance.Signature:function(day: TDate) => booleanday The date to test.Returns: If true the date will be disabled.",
+    name : "shouldDisableDate",
+    types: [{ default: "-", type: "function" }],
+  },
+  {
+    description:
+      "Disable specific year.Signature:function(year: TDate) => booleanyear The year to test.Returns: If true, the year will be disabled.",
+    name : "shouldDisableYear",
+    types: [{ default: "-", type: "function" }],
+  },
+  {
+    description:
+      "If true, days outside the current month are rendered:- if fixedWeekNumber is defined, renders days to have the weeks requested.- if fixedWeekNumber is not defined, renders day to fill the first and last week of the current month.- ignored if calendars equals more than 1 on range pickers.",
+    name : "showDaysOutsideCurrentMonth",
+    types: [{ default: false, type: "boolean" }],
+  },
+  {
+    description: "The props used for each component slot.",
+    name       : "slotProps",
+    types      : [{ default: "{}", type: "object" }],
+  },
+  {
+    description:
+      "Overridable component slots.See Slots API below for more details.",
+    name : "slots",
+    types: [{ default: "{}", type: "object" }],
+  },
+  {
+    description:
+      "The system prop that allows defining system overrides as well as additional CSS styles.See the `sx` page for more details.",
+    name : "sx",
+    types: [{ default: "-", type: "Array<function| object| boolean>| function| object" }],
+  },
+  {
+    description: "The selected value. Used when the component is controlled.",
+    name       : "value",
+    types      : [{ default: "-", type: "object" }],
+  },
+  {
+    description:
+      "The visible view. Used when the component view is controlled. Must be a valid option from views list.",
+    name : "view",
+    types: [{ default: "-", type: "day'| 'month'| 'year'" }],
   },
 ];
 CoreDatePicker.invalidProps = [];
