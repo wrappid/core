470f57585596b454ef19d938af22569c4296990e
PritamIT2023 <pritammondal8981@gmail.com>
2024-08-31 13:22:46 +0530

    feat(global): :sparkles: fix CoreDateRangepicker
    fix CoreDateRangepicker
    
    Ref: #344

--- package/components/inputs/CoreDateRangepicker.js
+++ package/components/inputs/CoreDateRangepicker.js
@@ -1,21 +1,18 @@
+/* eslint-disable etc/no-commented-out-code */
 // eslint-disable-next-line unused-imports/no-unused-imports, no-unused-vars
 import React from "react";
 
 // eslint-disable-next-line import/no-unresolved
-import { NativeDateRangePicker } from "@wrappid/native";
+import { NativeDateRangepicker } from "@wrappid/native";
 
-import CoreFormHelperText from "./CoreFormHelperText";
-import CoreClasses from "../../styles/CoreClasses";
+// import CoreFormHelperText from "./CoreFormHelperText";
+// import CoreClasses from "../../styles/CoreClasses";
 import CoreBox from "../layouts/CoreBox";
 
 export default function CoreDateRangepicker(props) {
   return (
     <CoreBox>
-      <NativeDateRangePicker {...props} />
-
-      <CoreFormHelperText styleClasses={[CoreClasses.LAYOUT.NO_MARGIN_P]}>
-        {props.helperText}
-      </CoreFormHelperText>
+      <NativeDateRangepicker {...props} />
     </CoreBox>
   );
 }
