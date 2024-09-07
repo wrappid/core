9c496cf8daa1775fb066b15d923705a3cb7beecb
PritamIT2023 <pritammondal8981@gmail.com>
2024-09-07 11:24:00 +0530

    feat(global): :sparkles: add formik as valid props
    add formik as valid props and clean-up valid props
    
    Ref: #342

--- package/components/inputs/CoreDatepicker.js
+++ package/components/inputs/CoreDatepicker.js
@@ -31,6 +31,10 @@ export default function CoreDatePicker(props) {
 }
 CoreDatePicker.validProps = [
   {
+    name : "formik",
+    types: [{ type: "object" }]
+  },
+  {
     name : "helperText",
     types: [{ type: "string" }]
   },
@@ -40,10 +44,9 @@ CoreDatePicker.validProps = [
     types      : [{ default: "", type: "string" }],
   },
   {
-    description:
-      "The format of the date in the UI. The value for the bindings will always be in the YYYY-MM-DD format. Leave empty to let the end-user locale define the format.",
-    name : "format",
-    types: [{ default: "", type: "string" }],
+    description: "The format of the date in the UI. The value for the bindings will always be in the YYYY-MM-DD format. Leave empty to let the end-user locale define the format.",
+    name       : "format",
+    types      : [{ default: "", type: "string" }],
   },
   {
     description: "A default value for the date picker.",
@@ -51,10 +54,9 @@ CoreDatePicker.validProps = [
     types      : [{ default: "", type: "string" }],
   },
   {
-    description:
-      "A label that describes the content of the date picker. e.g. \"Arrival date\".",
-    name : "label",
-    types: [{ default: "", type: "node" }],
+    description: "A label that describes the content of the date picker. e.g. \"Arrival date\".",
+    name       : "label",
+    types      : [{ default: "", type: "node" }],
   },
   {
     description: "Name of this input. Used as a reference in form data.",
@@ -62,10 +64,9 @@ CoreDatePicker.validProps = [
     types      : [{ type: "string" }],
   },
   {
-    description:
-      "One of the available MUI TextField variants. Possible values are outlined, filled or standard",
-    name : "variant",
-    types: [{ default: "outlined", type: "string" }],
+    description: "One of the available MUI TextField variants. Possible values are outlined, filled or standard",
+    name       : "variant",
+    types      : [{ default: "outlined", type: "string" }],
   },
   {
     description: "The size of the component. One of small, or medium.",
@@ -73,10 +74,9 @@ CoreDatePicker.validProps = [
     types      : [{ default: "small", type: "string" }],
   },
   {
-    description:
-      "Whether the button should occupy all available horizontal space.",
-    name : "fullWidth",
-    types: [{ type: "boolean" }],
+    description: "Whether the button should occupy all available horizontal space.",
+    name       : "fullWidth",
+    types      : [{ type: "boolean" }],
   },
   {
     description: "The date picker is disabled.",
@@ -206,22 +206,19 @@ CoreDatePicker.validProps = [
     types: [{ type: "function" }],
   },
   {
-    description:
-      "Callback fired when the selected sections change.Signature:function(newValue: FieldSelectedSections) => voidnewValue The new selected sections.",
-    name : "onSelectedSectionsChange",
-    types: [{ default: "-", type: "function" }],
+    description: "Callback fired when the selected sections change.Signature:function(newValue: FieldSelectedSections) => voidnewValue The new selected sections.",
+    name       : "onSelectedSectionsChange",
+    types      : [{ type: "function" }],
   },
   {
-    description:
-      "Callback fired on view change.Signature:function(view: TView) => voidview The new view.",
-    name : "onViewChange",
-    types: [{ default: "-", type: "function" }],
+    description: "Callback fired on view change.Signature:function(view: TView) => voidview The new view.",
+    name       : "onViewChange",
+    types      : [{ type: "function" }],
   },
   {
-    description:
-      "Callback fired on year change.Signature:function(year: TDate) => voidyear The new year.",
-    name : "onYearChange",
-    types: [{ default: "-", type: "function" }],
+    description: "Callback fired on year change.Signature:function(year: TDate) => voidyear The new year.",
+    name       : "onYearChange",
+    types      : [{ type: "function" }],
   },
   {
     description: "Control the popup or dialog open state.",
@@ -229,15 +226,14 @@ CoreDatePicker.validProps = [
     types      : [{ default: false, type: "boolean" }],
   },
   {
-    description:
-      "The default visible view. Used when the component view is not controlled. Must be a valid option from views list.",
-    name : "openTo",
-    types: [{ default: "-", defaultValue: ["day", "month", "year"], type: "string" }],
+    description: "The default visible view. Used when the component view is not controlled. Must be a valid option from views list.",
+    name       : "openTo",
+    types      : [{ defaultValue: ["day", "month", "year"], type: "string" }],
   },
   {
     description: "Force rendering in particular orientation.",
     name       : "orientation",
-    types      : [{ default: "-", type: "landscape'| 'portrait'" }],
+    types      : [{ type: "string", validValues: ["landscape", "portrait"] }],
   },
   {
     description: "If true, disable heavy animations.",
@@ -251,51 +247,57 @@ CoreDatePicker.validProps = [
     ],
   },
   {
-    description:
-      "The date used to generate the new value when both value and defaultValue are empty.",
-    name : "referenceDate",
-    types: [
+    description: "The date used to generate the new value when both value and defaultValue are empty.",
+    name       : "referenceDate",
+    types      : [
       {
         default:
-          "The closest valid date-time using the validation props, except callbacks like `shouldDisable<...>`.",
+  "The closest valid date-time using the validation props, except callbacks like `shouldDisable<...>`.",
         type: "object",
       },
     ],
   },
   {
-    description:
-      "Component displaying when passed loading true.Signature:function() => React.ReactNodeReturns: The node to render when loading.",
-    name : "renderLoading",
-    types: [{ type: "function" }],
+    description: "Component displaying when passed loading true.Signature:function() => React.ReactNodeReturns: The node to render when loading.",
+    name       : "renderLoading",
+    types      : [{ type: "function" }],
   },
   {
-    description:
-      "The currently selected sections. This prop accepts four formats: 1. If a number is provided, the section at this index will be selected. 2. If a string of type FieldSectionType is provided, the first section with that name will be selected. 3. If \"all\"  is provided, all the sections will be selected. 4. If null is provided, no section will be selected. If not provided, the selected sections will be handled internally.",
-    name : "selectedSections",
-    types: [
+    description: "The currently selected sections. This prop accepts four formats: 1. If a number is provided, the section at this index will be selected. 2. If a string of type FieldSectionType is provided, the first section with that name will be selected. 3. If \"all\"  is provided, all the sections will be selected. 4. If null is provided, no section will be selected. If not provided, the selected sections will be handled internally.",
+    name       : "selectedSections",
+    types      : [
       {
-        default: "-",
-        type   : "all'| 'day'| 'empty'| 'hours'| 'meridiem'| 'minutes'| 'month'| 'seconds'| 'weekDay'| 'year'| number",
+        type       : "string",
+        validValues: [
+          "all",
+          "day",
+          "empty",
+          "hours",
+          "meridiem",
+          "minutes",
+          "month",
+          "seconds",
+          "weekDay",
+          "year"
+        ]
       },
+      { type: "number" }
     ],
   },
   {
-    description:
-      "Disable specific date.Warning: This function can be called multiple times (for example when rendering date calendar, checking if focus can be moved to a certain date, etc.). Expensive computations can impact performance.Signature:function(day: TDate) => booleanday The date to test.Returns: If true the date will be disabled.",
-    name : "shouldDisableDate",
-    types: [{ default: "-", type: "function" }],
+    description: "Disable specific date.Warning: This function can be called multiple times (for example when rendering date calendar, checking if focus can be moved to a certain date, etc.). Expensive computations can impact performance.Signature:function(day: TDate) => booleanday The date to test.Returns: If true the date will be disabled.",
+    name       : "shouldDisableDate",
+    types      : [{ type: "function" }],
   },
   {
-    description:
-      "Disable specific year.Signature:function(year: TDate) => booleanyear The year to test.Returns: If true, the year will be disabled.",
-    name : "shouldDisableYear",
-    types: [{ default: "-", type: "function" }],
+    description: "Disable specific year.Signature:function(year: TDate) => booleanyear The year to test.Returns: If true, the year will be disabled.",
+    name       : "shouldDisableYear",
+    types      : [{ type: "function" }],
   },
   {
-    description:
-      "If true, days outside the current month are rendered:- if fixedWeekNumber is defined, renders days to have the weeks requested.- if fixedWeekNumber is not defined, renders day to fill the first and last week of the current month.- ignored if calendars equals more than 1 on range pickers.",
-    name : "showDaysOutsideCurrentMonth",
-    types: [{ default: false, type: "boolean" }],
+    description: "If true, days outside the current month are rendered:- if fixedWeekNumber is defined, renders days to have the weeks requested.- if fixedWeekNumber is not defined, renders day to fill the first and last week of the current month.- ignored if calendars equals more than 1 on range pickers.",
+    name       : "showDaysOutsideCurrentMonth",
+    types      : [{ default: false, type: "boolean" }],
   },
   {
     description: "The props used for each component slot.",
@@ -303,27 +305,19 @@ CoreDatePicker.validProps = [
     types      : [{ default: "{}", type: "object" }],
   },
   {
-    description:
-      "Overridable component slots.See Slots API below for more details.",
-    name : "slots",
-    types: [{ default: "{}", type: "object" }],
-  },
-  {
-    description:
-      "The system prop that allows defining system overrides as well as additional CSS styles.See the `sx` page for more details.",
-    name : "sx",
-    types: [{ default: "-", type: "Array<function| object| boolean>| function| object" }],
+    description: "Overridable component slots.See Slots API below for more details.",
+    name       : "slots",
+    types      : [{ default: "{}", type: "object" }],
   },
   {
     description: "The selected value. Used when the component is controlled.",
     name       : "value",
-    types      : [{ default: "-", type: "object" }],
+    types      : [{ type: "object" }],
   },
   {
-    description:
-      "The visible view. Used when the component view is controlled. Must be a valid option from views list.",
-    name : "view",
-    types: [{ default: "-", type: "day'| 'month'| 'year'" }],
+    description: "The visible view. Used when the component view is controlled. Must be a valid option from views list.",
+    name       : "view",
+    types      : [{ type: "string", validValues: ["day", "month", "year"] }],
   },
 ];
 CoreDatePicker.invalidProps = [];
