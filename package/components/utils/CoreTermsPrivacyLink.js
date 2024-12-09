ba6c668f5e9b13636b64dd7941bc7ee86c3ed488
Nilotpal Saha <nilotpal.saha00@gmail.com>
2024-12-09 14:37:03 +0530

    fix(core): :bug: terms and privacy link fix in app container
    terms and privacy link fix in app container
    
    Ref #no-ref

--- package/components/utils/CoreTermsPrivacyLink.js
+++ package/components/utils/CoreTermsPrivacyLink.js
@@ -3,37 +3,33 @@ import React from "react";
 
 // eslint-disable-next-line import/no-unresolved
 import { WrappidDataContext } from "@wrappid/styles";
-import { useSelector } from "react-redux";
 
 import CoreClasses from "../../styles/CoreClasses";
+import CoreBox from "../layouts/CoreBox";
 import CoreLink from "../navigation/CoreLink";
 
 export default function CoreTermsPrivacyLink() {
-  const { config } = React.useContext(WrappidDataContext);
-  
-  const auth = useSelector(state => state.auth);
-  const { accessToken } = auth;
-  let authenticated = accessToken ? true : false;
+  const { config: appConfig } = React.useContext(WrappidDataContext);
 
   return (
-    <>
+    <CoreBox styleClasses={[CoreClasses.FLEX.DIRECTION_ROW, CoreClasses.PADDING.PB2]}>
       <CoreLink
-        styleClasses={[CoreClasses?.MARGIN?.MR1]}
-        href={config?.wrappid?.helpLink}
-      >Help</CoreLink>
+        href={ appConfig?.wrappid?.helpLink || "#" }
+        target="_blank"
+        rel="noopener noreferrer"
+        styleClasses={[CoreClasses.COLOR.TEXT_PRIMARY]}>Help</CoreLink>
 
       <CoreLink
-        styleClasses={authenticated ? [] : [CoreClasses?.COLOR?.TEXT_WHITE]}
-        href={config?.wrappid?.privacyLink}
-      >Privacy</CoreLink>
+        href={ appConfig?.wrappid?.privacyLink || "#" }
+        target="_blank"
+        rel="noopener noreferrer"
+        styleClasses={[CoreClasses.COLOR.TEXT_PRIMARY, CoreClasses.PADDING.PL2]}>Privacy</CoreLink>
 
       <CoreLink
-        styleClasses={authenticated
-          ? [CoreClasses?.MARGIN?.ML1]
-          : [CoreClasses?.MARGIN?.ML1, CoreClasses?.COLOR?.TEXT_WHITE]
-        }
-        href={config?.wrappid?.termsLink}
-      >Terms</CoreLink>
-    </>
+        href={ appConfig?.wrappid?.termsLink || "#" }
+        target="_blank"
+        rel="noopener noreferrer"
+        styleClasses={[CoreClasses.COLOR.TEXT_PRIMARY, CoreClasses.PADDING.PL2]}>Terms</CoreLink>
+    </CoreBox>
   );
 }
